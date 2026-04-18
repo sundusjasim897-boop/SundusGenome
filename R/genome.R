e34a0a3bd5e50883ed94c94280e0496391fd22f4
Sundus Jasim <sundusjasim897@gmail.com>
2026-04-18 11:09:15 +0400

    Fixed export issue

--- R/genome.R
+++ R/genome.R
@@ -1,3 +1,13 @@
+#' Genome class
+#'
+#' This class stores basic information about a genome,
+#' including its name, number of chromosomes, and number of proteins.
+#'
+#' @slot genome_name the name of the genome
+#' @slot num_chromosomes number of chromosomes
+#' @slot num_proteins number of proteins
+#'
+#' @export
 setClass(
   "Genome",
   slots = list(
@@ -7,11 +17,20 @@ setClass(
   )
 )
 
+#' Average proteins per chromosome
+#'
+#' This function calculates the average number of proteins
+#' per chromosome for a Genome object.
+#'
+#' @param object a Genome object
+#' @return the average number of proteins per chromosome (rounded)
+#' @export
 setGeneric(
   "avgProteinPerCh",
   function(object) standardGeneric("avgProteinPerCh")
 )
 
+#' @export
 setMethod(
   "avgProteinPerCh",
   "Genome",
@@ -20,3 +39,4 @@ setMethod(
     return(round(avg))
   }
 )
+
